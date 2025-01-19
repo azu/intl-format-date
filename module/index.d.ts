25951ca088603a2c2215337a176964b8c15b683f
azu <azuciao@gmail.com>
2025-01-19 22:14:08 +0900

    rm

--- module/index.d.ts
+++
@@ -1,6 +0,0 @@
-export type FormatContext = {
-    timeZone: string;
-    locale: string;
-};
-export declare const formatDate: (context: FormatContext, date: Date, pattern: string) => string;
-//# sourceMappingURL=index.d.ts.map
